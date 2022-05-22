dc2a7e895315ce8dec697778232abb5f995200b0
GAYA <Gayay@academy-skills.com>
2022-05-22 13:47:56 +0300

    first commit

---
+++ src/main/java/com/moveo/epicure/EpicureApplication.java
@@ -0,0 +1,13 @@
+package com.moveo.epicure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moveo/epicure/EpicureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moveo.epicure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
