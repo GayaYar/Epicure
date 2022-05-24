8df57d258578a766ac1a190360ccbbcf736e67de
GAYA <Gayay@academy-skills.com>
2022-05-24 15:03:09 +0300

    created meal repo

---
+++ src/main/java/com/moveo/epicure/repo/MealRepo.java
@@ -0,0 +1,10 @@
+package com.moveo.epicure.repo;
+
+import com.moveo.epicure.entity.Meal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MealRepo extends JpaRepository<Meal, Integer> {
+
+}
--- src/main/java/com/moveo/epicure/repo/RestaurantRepoImpl.java
+++ src/main/java/com/moveo/epicure/repo/RestaurantRepoImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 public class RestaurantRepoImpl {
     @PersistenceContext
     private EntityManager entityManager;
-    
+
     public List<Restaurant> findOrderByPopularityLimitedTo(int limit) {
         return entityManager.createQuery("SELECT r FROM Restaurant r ORDER BY r.popularity DESC", Restaurant.class)
                 .setMaxResults(limit).getResultList();
