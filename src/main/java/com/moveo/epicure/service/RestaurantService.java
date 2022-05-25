fb08e53d20831eef2cff054b736e3ea7eca71849
GAYA <Gayay@academy-skills.com>
2022-05-25 11:22:43 +0300

    get popular in restaurant service

--- src/main/java/com/moveo/epicure/service/RestaurantService.java
+++ src/main/java/com/moveo/epicure/service/RestaurantService.java
@@ -3,11 +3,14 @@ package com.moveo.epicure.service;
 import com.moveo.epicure.dto.MealDTO;
 import com.moveo.epicure.dto.RestaurantDTO;
 import com.moveo.epicure.dto.RestaurantBriefDTO;
+import com.moveo.epicure.entity.Restaurant;
+import com.moveo.epicure.util.DtoMapper;
 import java.util.List;
 import java.util.Optional;
 import com.moveo.epicure.repo.MealRepo;
 import com.moveo.epicure.repo.RestaurantRepo;
 import com.moveo.epicure.repo.RestaurantRepoImpl;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,12 +24,18 @@ public class RestaurantService {
     private MealRepo mealRepo;
 
     public List<RestaurantBriefDTO> getPopulars(Integer amount) {
-        //to do
-        amount = (amount==null) ? 3 : amount;
-        return null;
+        List<Restaurant> populars;
+        if(amount == null || amount == 3) {
+            populars = restaurantRepo.findTop3ByOrderByPopularityDesc();
+        } else {
+            populars = restaurantRepoImpl.findOrderByPopularityLimitedTo(amount);
+        }
+        return populars.stream()
+                .map(restaurant -> {return DtoMapper.restaurantToBriefDto(restaurant);}).collect(Collectors.toList());
     }
 
-    public List<RestaurantBriefDTO> getAllSorted(Integer minPrice, Integer maxPrice, Boolean newest, Boolean popular, Integer distance, Boolean open, Integer rating) {
+    public List<RestaurantBriefDTO> getAllSorted(Integer minPrice, Integer maxPrice, Boolean newest, Boolean popular
+            , Integer distance, Boolean open, Integer rating) {
         //to do
         return null;
     }
