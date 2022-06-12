67a41c493a08e35d50fdb8904dc696084a8a3909
GAYA <Gayay@academy-skills.com>
2022-06-12 16:19:35 +0300

    controller logs

--- src/main/java/com/moveo/epicure/controller/RestaurantController.java
+++ src/main/java/com/moveo/epicure/controller/RestaurantController.java
@@ -8,6 +8,7 @@ import com.moveo.epicure.service.RestaurantService;
 import java.util.List;
 import java.util.Optional;
 import javax.validation.constraints.Min;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -19,16 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("restaurants")
+@Slf4j
 @Validated
 public class RestaurantController {
     private RestaurantService service;
-
+    private String fileName;
     public RestaurantController(RestaurantService service) {
         this.service = service;
+        fileName = "[RestaurantController:";
     }
 
     @GetMapping(value = "/popular")
     public ResponseEntity<List<RestaurantBriefDTO>> getPopularRestaurants(@RequestParam(required = false) @Min(0) Integer amount) {
+        log.debug(fileName+"getPopularRestaurants]");
         return ResponseEntity.ok(service.getPopulars(amount));
     }
 
@@ -38,14 +42,17 @@ public class RestaurantController {
             , @RequestParam(required = false) Double longitude, @RequestParam(required = false) Double latitude
             , @RequestParam(required = false) @Min(0) Integer distance, @RequestParam(required = false) Boolean open
             , @RequestParam(required = false) Integer rating) {
+        log.debug(fileName+"getRestaurants]");
         return ResponseEntity.ok(service.getAllSorted(minPrice, maxPrice, newest, longitude, latitude, distance
                 , open, rating));
     }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<RestaurantDTO> findById(@PathVariable @Min(0) Integer id) {
+        log.debug(fileName+"findById]");
         Optional<RestaurantDTO> optionalRestaurant = service.findById(id);
         if(optionalRestaurant.isEmpty()) {
+            log.error(fileName+"findById] restaurant with id: "+id+" was not found");
             throw new NotFoundException("restaurant");
         }
         return ResponseEntity.ok(optionalRestaurant.get());
@@ -53,8 +60,10 @@ public class RestaurantController {
 
     @GetMapping(value = "/meals/{id}")
     public ResponseEntity<MealDTO> getMeal(@PathVariable @Min(0) Integer id) {
+        log.debug(fileName+"getMeal]");
         Optional<MealDTO> optionalMeal = service.findMeal(id);
         if(optionalMeal.isEmpty()) {
+            log.error(fileName+"getMeal] meal with id: "+id+" was not found");
             throw new NotFoundException("meal");
         }
         return ResponseEntity.ok(optionalMeal.get());
