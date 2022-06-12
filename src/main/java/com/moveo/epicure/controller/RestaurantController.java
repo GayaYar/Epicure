f379f6e871ae78b95cbe6328a935e822f009562f
GAYA <Gayay@academy-skills.com>
2022-06-12 17:49:45 +0300

    get all second method for pageable

--- src/main/java/com/moveo/epicure/controller/RestaurantController.java
+++ src/main/java/com/moveo/epicure/controller/RestaurantController.java
@@ -9,6 +9,9 @@ import java.util.List;
 import java.util.Optional;
 import javax.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,6 +43,17 @@ public class RestaurantController {
             , @RequestParam(required = false) Integer rating) {
         return ResponseEntity.ok(service.getAllSorted(minPrice, maxPrice, newest, longitude, latitude, distance
                 , open, rating));
+    }
+
+    @GetMapping(value = "/pageable")
+    public ResponseEntity<List<RestaurantBriefDTO>> getPageableRestaurants(@RequestParam(required = false) @Min(0) Integer minPrice,
+            @RequestParam(required = false) @Min(0) Integer maxPrice, @RequestParam(required = false) Boolean newest
+            , @RequestParam(required = false) Double longitude, @RequestParam(required = false) Double latitude
+            , @RequestParam(required = false) @Min(0) Integer distance, @RequestParam(required = false) Boolean open
+            , @RequestParam(required = false) Integer rating, @RequestParam @Min(0) int page, @RequestParam @Min(0) int size) {
+        Pageable pageRequest = PageRequest.of(page, size);
+        return ResponseEntity.ok(service.getAllSortedPageable(minPrice, maxPrice, newest, longitude, latitude, distance
+                , open, rating, pageRequest));
     }
 
     @GetMapping(value = "/{id}")
