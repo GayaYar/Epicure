23799790e7e52f560ae0fca3aafe0cbef2f45ef5
GAYA <Gayay@academy-skills.com>
2022-06-13 15:00:26 +0300

    added signature field and query

--- src/main/java/com/moveo/epicure/repo/MealRepo.java
+++ src/main/java/com/moveo/epicure/repo/MealRepo.java
@@ -1,6 +1,7 @@
 package com.moveo.epicure.repo;
 
 import com.moveo.epicure.entity.Meal;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +12,6 @@ import org.springframework.stereotype.Repository;
 public interface MealRepo extends JpaRepository<Meal, Integer> {
     @Query("SELECT m FROM Meal m JOIN FETCH m.choices WHERE m.id=:id")
     Optional<Meal> findMealWithChoices(@Param("id") Integer id);
+
+    List<Meal> findBySignatureTrue();
 }
