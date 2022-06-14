c2598867b0ed1e55ab77ca85051a55b74498189b
GAYA <Gayay@academy-skills.com>
2022-06-14 12:43:05 +0300

    finished

--- src/test/java/com/moveo/epicure/mock/MockCustomer.java
+++ src/test/java/com/moveo/epicure/mock/MockCustomer.java
@@ -1,8 +1,12 @@
 package com.moveo.epicure.mock;
 
+import com.moveo.epicure.dto.LoginResponse;
 import com.moveo.epicure.entity.LoginAttempt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MockCustomer {
@@ -14,5 +18,25 @@ public class MockCustomer {
             attempts.add(new LoginAttempt("blocked@mail.com", LocalDateTime.now().minusMinutes((long) i)));
         }
         return attempts;
+    }
+
+    //necessary for limit login test
+    public List<LoginAttempt> getNotBlockedAttempt() {
+        List<LoginAttempt> attempts = new ArrayList<>(9);
+        for(int i=1; i<=9; i++) {
+            attempts.add(new LoginAttempt("mockCus@gmail.com", LocalDateTime.now().minusMinutes((long) i*10)));
+        }
+        return attempts;
+    }
+
+    public LoginResponse mockResponse() {
+        String jwts = Jwts.builder()
+                .setIssuer("epicure")
+                .setSubject("9")
+                .claim("customerName", "mock cus")
+                .setIssuedAt(new Date())
+                .signWith(Keys.hmacShaKeyFor("sdhfhsdfggusdfkuygsdufggfbgvtsdgfurfbocvajnrgaiuetjrbg ".getBytes()))
+                .compact();
+        return new LoginResponse("mock cus", jwts);
     }
 }
--- src/test/java/com/moveo/epicure/service/CustomerServiceTest.java
+++ src/test/java/com/moveo/epicure/service/CustomerServiceTest.java
@@ -8,6 +8,7 @@ import com.moveo.epicure.dto.LoginInfo;
 import com.moveo.epicure.entity.Cart;
 import com.moveo.epicure.entity.ChosenMeal;
 import com.moveo.epicure.entity.Customer;
+import com.moveo.epicure.entity.LoginAttempt;
 import com.moveo.epicure.exception.AccountBlockedException;
 import com.moveo.epicure.mock.MockCustomer;
 import com.moveo.epicure.repo.AttemptRepo;
@@ -15,6 +16,8 @@ import com.moveo.epicure.repo.CartRepo;
 import com.moveo.epicure.repo.ChosenMealRepo;
 import com.moveo.epicure.repo.CustomerRepo;
 import com.moveo.epicure.repo.MealRepo;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -51,6 +54,8 @@ class CustomerServiceTest {
     private ArgumentCaptor<ChosenMeal> chosenMealArgumentCaptor;
     @Captor
     private ArgumentCaptor<Customer> customerArgumentCaptor;
+    @Captor
+    private ArgumentCaptor<LoginAttempt> attemptArgumentCaptor;
 
     /**
      * instantiates the necessary fields for the tests
@@ -71,10 +76,47 @@ class CustomerServiceTest {
 
     //login tests with limit
     @Test
-    void loginThrowsExceptionWhenAccountBlocked() {
+    void loginWhenBlockedThrowsExceptionAndDoesNotSaveAttempt() {
         LoginInfo info = new LoginInfo("blocked@mail.com", "a-password");
         Mockito.when(customerRepo.existsByEmail("blocked@mail.com")).thenReturn(true);
         Mockito.when(attemptRepo.findTop9ByMailOrderByTimeDesc("blocked@mail.com")).thenReturn(mockCustomer.getBlockedAttempt());
-        assertThatThrownBy(()->{service.login(info);}).isInstanceOf(AccountBlockedException.class);
+        try {
+            service.login(info);
+        }catch (Exception e) {
+            assertEquals(e.getClass(), AccountBlockedException.class);
+            Mockito.verify(attemptRepo, Mockito.times(0)).save(Mockito.any());
+        }
+    }
+
+    //login tests with limit
+    @Test
+    void loginSuccessful() {
+        String email = "mockCus@gmail.com";
+        String password = "12345678";
+        LoginInfo info = new LoginInfo(email, password);
+        Mockito.when(customerRepo.existsByEmail(email)).thenReturn(true);
+        Mockito.when(attemptRepo.findTop9ByMailOrderByTimeDesc(email)).thenReturn(mockCustomer.getNotBlockedAttempt());
+        Mockito.when(passwordEncoder.encode(password)).thenReturn(password);
+        Mockito.when(customerRepo.findByEmailAndPassword(email, password)).thenReturn(Optional.of(new Customer(1, "bob", email, password)));
+        assertEquals(service.login(info), Optional.of(mockCustomer.mockResponse()));
+    }
+
+    //login tests with limit
+    @Test
+    void loginFailedReturnsEmptyAndSavesAttempt() {
+        String email = "mockCus@gmail.com";
+        String password = "12345678";
+        LoginInfo info = new LoginInfo(email, password);
+        Mockito.when(customerRepo.existsByEmail(email)).thenReturn(true);
+        Mockito.when(attemptRepo.findTop9ByMailOrderByTimeDesc(email)).thenReturn(mockCustomer.getNotBlockedAttempt());
+        Mockito.when(passwordEncoder.encode(password)).thenReturn(password);
+        Mockito.when(customerRepo.findByEmailAndPassword(email, password)).thenReturn(Optional.empty());
+        service.login(info);
+        Mockito.verify(attemptRepo, Mockito.times(1)).save(attemptArgumentCaptor.capture());
+        LoginAttempt captorValue = attemptArgumentCaptor.getValue();
+        LoginAttempt expected = new LoginAttempt(email, LocalDateTime.now());
+        assertTrue(captorValue.getMail().equals(expected.getMail()) &&
+                Duration.between(captorValue.getTime(), expected.getTime()).toMinutes()<1);
+        assertEquals(service.login(info), Optional.empty());
     }
 }
