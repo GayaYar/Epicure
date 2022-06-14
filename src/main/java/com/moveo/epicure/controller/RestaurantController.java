14c5a057d88b669f0311823aa762d5de63a971a1
GAYA <Gayay@academy-skills.com>
2022-06-14 17:51:06 +0300

    fake add restaurant in controller

--- src/main/java/com/moveo/epicure/controller/RestaurantController.java
+++ src/main/java/com/moveo/epicure/controller/RestaurantController.java
@@ -12,6 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -57,6 +59,11 @@ public class RestaurantController {
             throw new NotFoundException("meal");
         }
         return ResponseEntity.ok(optionalMeal.get());
+    }
+
+    @PutMapping
+    public ResponseEntity<RestaurantDTO> addRestaurant(@RequestBody RestaurantDTO restaurantDTO) {
+        return ResponseEntity.ok(service.addRestaurant(restaurantDTO));
     }
 
 }
