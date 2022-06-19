98a48b5e97882966a6b3262b68c4ef6b465cdf93
GAYA <Gayay@academy-skills.com>
2022-06-19 12:23:27 +0300

    mail sender config and dependancies

---
+++ src/main/java/com/moveo/epicure/aws/MailConfig.java
@@ -0,0 +1,32 @@
+package com.moveo.epicure.aws;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.cloud.aws.mail.simplemail.SimpleEmailServiceMailSender;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.MailSender;
+
+@Configuration
+public class MailConfig {
+
+    public AWSStaticCredentialsProvider awsCredentials() {
+        BasicAWSCredentials credentials =
+                new BasicAWSCredentials("your-aws-access-key", "your-aws-access-secret");
+        return new AWSStaticCredentialsProvider(credentials);
+    }
+
+    @Bean
+    public AmazonSimpleEmailService getAmazonSimpleEmailService() {
+        return AmazonSimpleEmailServiceClientBuilder.standard().withCredentials(awsCredentials())
+                .withRegion("us-east-1").build();
+    }
+
+    @Bean
+    public MailSender mailSender(AmazonSimpleEmailService amazonSimpleEmailService) {
+        return new SimpleEmailServiceMailSender(amazonSimpleEmailService);
+    }
+
+}
